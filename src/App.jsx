7c1ad603b42dd83b62fbf5300e2d0de40d12bf0c
Ardian Setya <113000649+ardiansetya@users.noreply.github.com>
2024-12-02 14:54:48 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { UseFetchData } from "./hooks/useFetchData";
+import Form from "./components/Form";
+
+const App = () => {
+  const { posts } = UseFetchData();
+
+  const renderData = () => {
+    return posts.map((data, index) => {
+      return (
+        <div key={index} className="overflow-x-auto">
+          <table className="min-w-full text-sm text-gray-700">
+            <thead className="border-b border-gray-300 bg-gray-50">
+              <tr>
+                <th className="px-6 py-3 text-left font-medium">ID</th>
+                <th className="px-6 py-3 text-left font-medium">Title</th>
+                <th className="px-6 py-3 text-left font-medium">Body</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="hover:bg-gray-100 transition-colors">
+                <td className="px-6 py-4 border-b border-gray-200">
+                  {data.id}
+                </td>
+                <td className="px-6 py-4 border-b border-gray-200">
+                  {data.title}
+                </td>
+                <td className="px-6 py-4 border-b border-gray-200">
+                  {data.body}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
+    });
+  };
+
+  return <>{renderData()}
+  <Form/>
+  </>;
+};
+
+export default App;
